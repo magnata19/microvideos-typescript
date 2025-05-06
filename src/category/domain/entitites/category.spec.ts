23186d0d6c6dfd4850b3166aa46c4ba951850547
Davidson <dehpacifico@gmail.com>
2025-05-05 22:27:22 -0300

    adicionando atributos a categoria e testes unitarios

--- src/category/domain/entitites/category.spec.ts
+++ src/category/domain/entitites/category.spec.ts
@@ -1,10 +1,60 @@
 import Category from "./category"
+import { omit } from 'lodash'
 
-describe("Test of category", () => {
+describe("Category Unit Test", () => {
 
   test("testing the constructor of category", () => {
-    const category = new Category("Davidson");
-    expect(category.name).toBe("Davidson");
-    expect(category.name).toBeDefined()
+    let category = new Category({
+      name: "Davidson"
+    });
+    let props = omit(category.props, 'created_at');
+    expect(props).toStrictEqual({
+      name: "Davidson",
+      description: null,
+      is_active: true
+    });
+
+    let created_at = new Date();
+    category = new Category({
+      name: "Davidson",
+      description: "Some description",
+      is_active: false,
+      created_at
+    })
+    expect(category.props).toStrictEqual({
+      name: "Davidson",
+      description: "Some description",
+      is_active: false,
+      created_at
+    });
+
+    category = new Category({
+      name: "Davidson",
+      description: "Other description",
+    })
+    expect(category.props).toMatchObject({
+      name: "Davidson",
+      description: "Other description"
+    });
+
+    category = new Category({
+      name: "Davidson",
+      is_active: true
+    })
+    expect(category.props).toMatchObject({
+      name: "Davidson",
+      is_active: true
+    });
+
+    created_at = new Date()
+    category = new Category({
+      name: "Davidson",
+      created_at
+    })
+    expect(category.props).toMatchObject({
+      name: "Davidson",
+      created_at
+    });
+
   })
 })

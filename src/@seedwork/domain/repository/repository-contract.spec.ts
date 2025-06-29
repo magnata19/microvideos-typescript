c80beaad87ed165d6a71f04ba2a2d66b6038cf01
Davidson <dehpacifico@gmail.com>
2025-06-29 20:25:02 -0300

    creating SearchResult and itself unit test

--- src/@seedwork/domain/repository/repository-contract.spec.ts
+++ src/@seedwork/domain/repository/repository-contract.spec.ts
@@ -1,4 +1,4 @@
-import { SearchParam } from "./repository-contract"
+import { SearchParam, SearchResult } from "./repository-contract"
 
 describe("SearchParams Unit tests", () => {
   test('page prop', () => {
@@ -129,5 +129,79 @@ describe("SearchParams Unit tests", () => {
     arrange.forEach((i) => {
       expect(new SearchParam({ filter: i.filter }).filter).toBe(i.expected);
     })
+  })
+})
+
+describe('SearchResult Unit tests', () => {
+  test('constructor props', () => {
+    let result = new SearchResult({
+      items: ['entity1', 'entity2'] as any,
+      total: 4,
+      current_page: 1,
+      per_page: 2,
+      sort: null,
+      sort_dir: null,
+      filter: null
+    })
+
+    expect(result.toJSON()).toStrictEqual({
+      items: ['entity1', 'entity2'] as any,
+      total: 4,
+      current_page: 1,
+      per_page: 2,
+      last_page: 2,
+      sort: null,
+      sort_dir: null,
+      filter: null
+    })
+
+    result = new SearchResult({
+      items: ['entity1', 'entity2'] as any,
+      total: 4,
+      current_page: 1,
+      per_page: 2,
+      sort: 'name',
+      sort_dir: 'asc',
+      filter: 'test'
+    })
+
+    expect(result.toJSON()).toStrictEqual({
+      items: ['entity1', 'entity2'] as any,
+      total: 4,
+      current_page: 1,
+      per_page: 2,
+      last_page: 2,
+      sort: 'name',
+      sort_dir: 'asc',
+      filter: 'test'
+    })
+  })
+
+  it('should set last_page 1 when per_page field is greater than total', () => {
+    let result = new SearchResult({
+      items: [] as any,
+      total: 4,
+      current_page: 1,
+      per_page: 15,
+      sort: 'name',
+      sort_dir: 'asc',
+      filter: 'test'
+    })
+
+    expect(result.last_page).toBe(1);
+  })
+
+  test('last_page prop when total is not a multiple of per_page', () => {
+    let result = new SearchResult({
+      items: [] as any,
+      total: 101,
+      current_page: 1,
+      per_page: 20,
+      sort: 'name',
+      sort_dir: 'asc',
+      filter: 'test'
+    })
+
+    expect(result.last_page).toBe(6);
   })
 })
--- src/category/infra/repository/category-in-memory.repository.ts
+++ src/category/infra/repository/category-in-memory.repository.ts
@@ -2,6 +2,6 @@ import { InMemorySearchableRepository } from "@seedwork/domain/repository/in-mem
 import Category from "category/domain/entitites/category";
 import CategoryInterface from "category/domain/repository/category.repository";
 
-export default class CategoryInMemoryRepository extends InMemorySearchableRepository<Category> implements CategoryInterface {
+export default class CategoryInMemoryRepository extends InMemorySearchableRepository<Category, any, any> implements CategoryInterface {
 
 }
